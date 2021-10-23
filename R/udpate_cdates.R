896aa97df3b6773859bfc86cdc95660a735b2029
tilschuenemann <til.schuenemann@mailbox.org>
2021-10-23 11:18:56 +0200

    add option to to use wide or short ledger as argument

--- R/udpate_cdates.R
+++ R/udpate_cdates.R
@@ -19,17 +19,35 @@
 #' @importFrom dplyr relocate
 #' @importFrom rlang .data
 
-udpate_cdates <- function(wide_ledger) {
-  wide_ledger <- read_delim("wide_ledger.csv",
-    ";",
-    escape_double = FALSE, locale = locale(encoding = "UTF-8", decimal_mark = ","),
-    trim_ws = TRUE, col_types = cols()
-  )
+udpate_cdates <- function(ledger_format) {
 
-  wide_ledger$date_custom <- ymd(.data$date_custom)
+  if(ledger_format!= "wide" && ledger_format != "short" &&  ledger_format != "both"){
+    stop("wrong ledger_format. please use either 'short','wide' or 'both'")
+  }
+
+  if(ledger_format == "wide"){
+    ledger <- read_delim("wide_ledger.csv",
+                              ";",
+                              escape_double = FALSE, locale = locale(encoding = "UTF-8", decimal_mark = ","),
+                              trim_ws = TRUE, col_types = cols()
+    )
+  } else if (ledger_format == "short"){
+    ledger <- read_delim("short_ledger.csv",
+                              ";",
+                              escape_double = FALSE, locale = locale(encoding = "UTF-8", decimal_mark = ","),
+                              trim_ws = TRUE, col_types = cols()
+    )
+  } else if (ledger_format== "both"){
+    update_cdates("short")
+    update_cdates("wide")
+
+  }
+
+
+  ledger$date_custom <- ymd(.data$date_custom)
 
   # TODO date temp is not a clean solution
-  wide_ledger <- wide_ledger %>%
+  ledger <- ledger %>%
     select(-.data$year, -.data$quarter, -.data$month) %>%
     mutate(
       date_temp = coalesce(.data$date_custom, .data$date),
@@ -40,6 +58,10 @@ udpate_cdates <- function(wide_ledger) {
     select(-.data$date_temp) %>%
     relocate(c(.data$year,.data$quarter,.data$month),.after = .data$date_custom)
 
-  readr::write_excel_csv2(wide_ledger, "wide_ledger.csv")
+  if(ledger_format == "wide"){
+    readr::write_excel_csv2(ledger, "wide_ledger.csv")
+  } else if(ledger_format == "short"){
+    readr::write_excel_csv2(ledger, "short_ledger.csv")
+  }
   print("updated custom dates")
 }
