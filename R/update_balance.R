9580523a5729b3ac5e0e5f648b06be2b545470ff
tilschuenemann <til.schuenemann@mailbox.org>
2021-10-23 11:20:10 +0200

    added function

---
+++ R/update_balance.R
@@ -0,0 +1,46 @@
+#' Title
+#'
+#' @export
+#'
+#' @importFrom readr read_delim
+#' @importFrom readr locale
+#' @importFrom readr cols
+#' @importFrom dplyr filter
+#' @importFrom dplyr select
+#' @importFrom dplyr rename
+#' @importFrom dplyr mutate
+#' @importFrom dplyr arrange
+#' @importFrom dplyr slice
+#' @importFrom readr write_excel_csv2
+#' @importFrom dplyr n
+update_balance <- function() {
+
+  short_ledger <- read_delim("short_ledger.csv",
+                             ";",
+                             escape_double = FALSE, locale = locale(encoding = "UTF-8", decimal_mark = ","),
+                             trim_ws = TRUE, col_types = cols()
+  )
+
+  short_ledger <- short_ledger %>%
+    arrange(date)
+
+  initial_balance <- short_ledger %>%
+    filter(recipient == "LEDGER INITIAL BALANCE") %>%
+    select(balance) %>%
+    rename(amount = 1)
+
+
+
+  balances <- data.frame(amount = c(initial_balance$amount, short_ledger$amount))
+
+  balances <- balances %>%
+    mutate(balance = cumsum(amount)) %>%
+    slice(2:n())
+
+  short_ledger$balance <- balances$balance
+
+  write_excel_csv2(short_ledger, "short_ledger.csv")
+
+}
+
+
