5cb27faf09e37b8c152e48a37dba47ee0a0596ab
tilschuenemann <til.schuenemann@mailbox.org>
2021-11-13 13:56:06 +0100

    added test case for missing maptab

--- tests/testthat/test-check-maptab.R
+++ tests/testthat/test-check-maptab.R
@@ -15,3 +15,7 @@ test_that("mapping_table has no rows", {
   expect_error(ledger:::check_maptab(test_path("maptab_malformed3.csv")),
                "mapping_table has no rows")
 })
+
+test_that("mapping_table doesnt exist", {
+  expect_silent(ledger:::check_maptab(test_path("missing_maptab.csv")))
+})
