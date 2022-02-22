dded2f1a435f54f4189e5134b4fdac1199f10169
schuenemann <til.schuenemann@mailbox.org>
2022-02-22 13:23:09 +0100

    removed comments

--- R/create_ledger.R
+++ R/create_ledger.R
@@ -1,4 +1,4 @@
-#' Title
+#' Creates the standard ledger
 #'
 #' @param path_to_export Path to CSV export file.
 #' @param export_type Specify which bank export will be used.
@@ -7,24 +7,19 @@
 #' @export
 #'
 #' @import readr
-create_ledger <- function(path_to_ledgerdir, export_type, path_to_export) {
+create_ledger <- function(path_to_ledgerdir, path_to_export, export_type) {
 
   # test inputs and prepare output path
   check_ledger_dir(path_to_ledgerdir)
   check_export_type(export_type)
   ledger_path <- paste0(path_to_ledgerdir, "ledger.csv")
 
-  # bank specific cleaning and creating short ledger format
-  ledger <- ledger:::transform_export(path_to_export, export_type)
+  ledger <- transform_export(path_to_export, export_type)
 
-  # write and print
   write_excel_csv2(ledger, ledger_path)
   print("wrote ledger")
 
-  # write balance
-  ledger:::setup_balance(path_to_export, export_type, path_to_ledgerdir)
-
-  # update balance and maptab
+  setup_balance(path_to_export, export_type, path_to_ledgerdir)
   update_ledger(path_to_ledgerdir)
   sort_ledger(path_to_ledgerdir)
 
