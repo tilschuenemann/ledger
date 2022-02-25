217213ee2feeea883024e03ac89557afe275eb61
schuenemann <til.schuenemann@mailbox.org>
2022-02-25 11:48:23 +0100

    added handling for existing ledger

--- R/create_ledger.R
+++ R/create_ledger.R
@@ -7,12 +7,21 @@
 #' @export
 #'
 #' @import readr
+#' @import utils
 create_ledger <- function(path_to_ledgerdir, path_to_export, export_type) {
 
   # test inputs and prepare output path
   check_ledger_dir(path_to_ledgerdir)
   check_export_type(export_type)
   ledger_path <- paste0(path_to_ledgerdir, "ledger.csv")
+
+  if(file.exists(ledger_path)){
+    overwrite <- menu(c("no","yes"),title="ledger already exists. Do you want to overwrite it?")
+
+    if(overwrite==1){
+      stop("not overwriting ledger. stopping.")
+    }
+  }
 
   ledger <- transform_export(path_to_export, export_type)
 
--- tests/testthat/test-create-ledger.R
+++ tests/testthat/test-create-ledger.R
@@ -1,11 +1,4 @@
 test_that("create ledger", {
-
-
-  expect_output(  create_ledger(path_to_ledgerdir = test_path("./"),
-                                 export_type = "dkb",
-                                 path_to_export = test_path("dkb_example_export.csv")),
-                 "wrote ledger")
-
   expect_true(file.exists(test_path("ledger.csv")))
 
 })
